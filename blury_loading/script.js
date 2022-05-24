336cc2e4ca592630c1b133210d7c5c366622ada7
konain1 <neeruali99@gmail.com>
2022-05-24 13:21:01 +0530

    fjsdhfl

---
+++ blury_loading/script.js
@@ -0,0 +1,28 @@
+const bg = document.querySelector('.bg');
+
+const text = document.querySelector('.loading_text');
+
+let load = 0;
+let int = setInterval(blurring,30);
+
+
+const scale = (num , in_min, in_max, out_min, out_max) =>{
+
+
+    return (num-in_min) * (out_max - out_min) / (in_max-in_min) + out_min;
+}
+
+function blurring(){
+    load++;
+
+
+    if(load > 99){
+        clearInterval(int);
+    }
+    text.innerText = `${load}%`;
+
+    text.style.opacity =  scale(load,0,100,1,0);
+
+    // bg.style.filter = 'blur(${scale(load,0,100,30,0)}px)';
+}
+
